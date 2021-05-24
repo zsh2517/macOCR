28155e7bf53277e0bd1ec7381ccf711efdaad434
Adam Zethraeus <adam@zethrae.us>
2021-05-23 20:46:39 -0700

    better args and screen recording

--- Sources/ocr/CLIArgs.swift
+++ Sources/ocr/CLIArgs.swift
@@ -4,13 +4,13 @@ import Foundation
 struct CLIArgs {
     struct ocr: ParsableArguments {
 
-        @Flag(name: [.customShort("c"), .long], help: "[C]apture screenshot.")
+        @Flag(name: [.customShort("c"), .long], help: "Capture screenshot.")
         var capture: Bool = false
 
-        @Flag(name: [.customShort("s"), .long], help: "Read [s]tdin binary data.")
+        @Flag(name: [.customShort("s"), .long], help: "Read stdin binary data.")
         var stdin: Bool = false
 
-        @Option(name: [.customShort("i")], help: "Path to [i]nput image.", completion: CompletionKind.file(extensions: ["gif", "png", "jpg", "webp", "tiff"]))
+        @Option(name: [.customShort("i")], help: "Path to input image.", completion: CompletionKind.file(extensions: ["gif", "png", "jpg", "webp", "tiff"]))
         var input: String?
 
 
